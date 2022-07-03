6bae93a948bfcd27e048331da0c5c7005c37e9b9
Renata <renatamagno@live.com>
2022-07-03 01:42:16 -0300

    :recycle: refactor app file

---
+++ back-end/prisma/migrations/20220702194943_init/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Task` (
+    `id` VARCHAR(191) NOT NULL,
+    `status` VARCHAR(191) NOT NULL DEFAULT 'pendente',
+    `userId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Task` ADD CONSTRAINT `Task_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,24 +1,31 @@
+import express from 'express';
 import cors = require('cors');
-import express = require('express');
 // import errorHandler from './middlewares/errorHandler';
-// import routes from './routes';
-
+import routes from './routes';
 
 class App {
   public app: express.Express;
 
   constructor() {
     this.app = express();
+    this.config();
+  }
+
+  private config():void {
     this.app.use(cors());
     this.app.use(express.json());
-    // this.app.use(routes);
+    this.app.use(routes);
     // this.app.use(errorHandler);
   }
 
   public start(PORT: string | number):void {
-    console.log(`Executando na porta ${PORT}`);
+    console.log(`Rodando na porta ${PORT}`);
+    
     this.app.listen(PORT);
   }
 }
 
 export { App };
+
+// A execução dos testes de cobertura depende dessa exportação
+export const { app } = new App();
