682a5626155c3e2f6d1535f6c28389cdf8126703
Renata <renatamagno@live.com>
2022-07-04 19:18:58 -0300

    :sparkles: Create tokenValidation middleware

--- back-end/src/middlewares/index.ts
+++ back-end/src/middlewares/index.ts
@@ -1,16 +1,19 @@
 import { ErrorRequestHandler, RequestHandler } from 'express';
 import HttpError from '../utils/errors/HttpError';
 import joi from 'joi';
-import { ILogin } from '../interfaces';
 import { BadRequest } from '../utils/errors';
+import { ILogin } from '../interfaces/loginInterface';
+import { Unauthorized } from '../utils/errors/Unauthorized';
+import Jwt from '../utils/auth/jwt';
+import { JsonWebTokenError } from 'jsonwebtoken';
 
 export default class Middlewares {
   public static error: ErrorRequestHandler = (err, _req, res, _next) => {
     if (err instanceof HttpError) {
       const { httpCode, message } = err;
-
       return res.status(httpCode).json({ message });
-    }
+    };
+    console.log('error >>>>>>', err);
     return res.status(500).json({ message: 'Internal server error' });
   };
 
@@ -24,5 +27,18 @@ export default class Middlewares {
 
     if(error) return next(new BadRequest(error.message));
     next();
+  };
+
+  public static tokenValidation: RequestHandler = (req, _res, next) => {
+    const { authorization } = req.headers;
+    if(!authorization) return next(new Unauthorized('Token is required', 401));
+
+    try {
+      const user = Jwt.validate(authorization);
+      req.body.user = user.id;
+      next();
+    } catch (error) {
+      return next(new Unauthorized('Invalid token', 401));
+    };
   };
 }
