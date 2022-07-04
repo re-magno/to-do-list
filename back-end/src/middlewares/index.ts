ab9d7f157c35f484a9d148a05109e3aa9f2097ab
Renata <renatamagno@live.com>
2022-07-03 21:30:23 -0300

    :sparkles: Add validation middleware for '/login' endpoint

--- back-end/src/middlewares/index.ts
+++ back-end/src/middlewares/index.ts
@@ -1,5 +1,8 @@
-import { ErrorRequestHandler } from 'express';
+import { ErrorRequestHandler, RequestHandler } from 'express';
 import HttpError from '../utils/errors/HttpError';
+import joi from 'joi';
+import { ILogin } from '../interfaces';
+import { BadRequest } from '../utils/errors';
 
 export default class Middlewares {
   public static error: ErrorRequestHandler = (err, _req, res, _next) => {
@@ -9,5 +12,17 @@ export default class Middlewares {
       return res.status(httpCode).json({ message });
     }
     return res.status(500).json({ message: 'Internal server error' });
+  };
+
+  private static _loginSchema = joi.object({
+    email: joi.string().email().required(),
+  });
+
+  public static loginValidation: RequestHandler = (req, _res, next) => {
+    const { email } = req.body as ILogin;
+    const { error } = Middlewares._loginSchema.validate({ email });
+
+    if(error) return next(new BadRequest(error.message));
+    next();
   };
 }
